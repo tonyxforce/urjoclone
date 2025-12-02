25ea738cc91a1c885ac76253a38fc13b762db6bb
tonyxforce <horich.dominik@gmail.com>
2025-12-02 12:45:19 +0100

    Add some extra functions for future functionality

--- puzzle.js
+++ puzzle.js
@@ -3,12 +3,18 @@ class Puzzle {
     decodedPuzzle;
     #mistakes = 0;
     #completedCells = 0;
-    #mistakeCallback = function(){};
+    #shownCellsIDs = [];
+    #isCreativeMode = false;
+    #mistakeCallback = function () { };
     #winCallback = function () { };
 
     // Class constructor
     // @Param {string} puzzleString - The puzzle string to decode
     constructor(puzzleString) {
+        if (typeof puzzleString == "number") {
+            puzzleString = new Array(puzzleString ** 2).fill("0").join("");
+            this.#isCreativeMode = true;
+        }
         this.puzzleString = puzzleString;
         const base36Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
         const result = [];
@@ -16,7 +22,15 @@ class Puzzle {
             result.push(base36Chars.indexOf(puzzleString.charAt(i)));
         }
         this.decodedPuzzle = result;
-    }
+        this.decodedPuzzle.forEach((color, index) => {
+            if (color == 1) {
+                this.#shownCellsIDs.push([index, "blue"]);
+            };
+            if (color == 3) {
+                this.#shownCellsIDs.push([index, "red"]);
+            };
+        });
+    };
 
     // Set the callback function to be called when the puzzle is won
     // @Param {function} callback - The callback function
@@ -24,15 +38,85 @@ class Puzzle {
         this.#winCallback = callback;
     }
 
+    // Get an array of the cell IDs that the player has successfully selected as a color
+    // @Returns {Array<Array<number, string>>} - An array of value pairs [cellID, color]
+    getShownCells() {
+        return this.#shownCellsIDs;
+    }
+
+    // Get shown cell colors as columns
+    // @Returns {Array<"red" | "blue" | "white">} - An array of columns, each column is an array of cell colors
+    getShownColumns() {
+        var columns = [];
+        for (let x = 0; x < this.getSize(); x++) {
+            columns.push([]);
+            for (let y = 0; y < this.getSize(); y++) {
+                var cellId = y * this.getSize() + x;
+                var color = this.#shownCellsIDs.find(pair => pair[0] == cellId);
+                columns[x].push(color ? color[1] : "white");
+            }
+        }
+        return columns;
+    }
+
+    // Get shown cell colors as columns
+    // @Returns {Array<"red" | "blue" | "white">} - An array of rows, each row is an array of cell colors
+    getShownRows() {
+        var rows = [];
+        for (let y = 0; y < this.getSize(); y++) {
+            rows.push([]);
+            for (let x = 0; x < this.getSize(); x++) {
+                var cellId = y * this.getSize() + x;
+                var color = this.#shownCellsIDs.find(pair => pair[0] == cellId);
+                rows[y].push(color ? color[1] : "white");
+            }
+        }
+        return rows;
+    }
+
+    // Randomly rotates, mirrors, or inverts the puzzle to create a new variation
+    // @Param {mirror: boolean, invert: boolean, rotate: number} options - Options for shuffling
+    shufflePuzzle(options) {
+        var rotationAmount = options.rotate ?? Math.floor(Math.random() * 3);
+        const doMirror = options.mirror ?? Math.round(Math.random());
+        const doInvert = options.invert ?? Math.round(Math.random());
+        if (doInvert)
+            this.decodedPuzzle = this.decodedPuzzle.map(value => {
+                if (value === 1) return 3;
+                if (value === 3) return 1;
+                return value;
+            });
+        if (doMirror) {
+            const size = this.getSize();
+            const mirrored = [];
+            for (let row = 0; row < size; row++) {
+                for (let col = size - 1; col >= 0; col--) {
+                    mirrored.push(this.decodedPuzzle[row * size + col]);
+                }
+            }
+            this.decodedPuzzle = mirrored;
+        }
+        const size = this.getSize();
+        for (let i = 0; i < rotationAmount; i++) {
+            const rotated = [];
+            for (let r = 0; r < size; r++) {
+                for (let c = 0; c < size; c++) {
+                    rotated.push(this.decodedPuzzle[(size - c - 1) * size + r]);
+                }
+            }
+            this.decodedPuzzle = rotated;
+        }
+    }
+
     // Set the callback function to be called when a mistake is made
     // @Param {function} callback - The callback function
-    setMistakeCallback(callback){
+    setMistakeCallback(callback) {
         this.#mistakeCallback = callback;
     }
 
     // Get number of mistakes made by the player
     // @Returns {number} - number of mistakes
-    getMistakeCount(){
+    getMistakeCount() {
         return this.#mistakes;
     }
 
@@ -97,26 +181,26 @@ class Puzzle {
 
     // Render the puzzle into an HTML element
     // @Param {string} elementId - The id of the HTML element to render the puzzle into
-    renderPuzzle(elementId) {
+    render(elementId) {
         var gameHolder = document.getElementById(elementId);
         gameHolder.innerHTML = "";
 
         var size = this.getSize();
         for (let x = 0; x < size; x++) {
-        
+
             var cellRow = document.createElement("div");
             cellRow.classList.add("cell-row");
-        
+
             for (let y = 0; y < size; y++) {
                 var id = x * size + y;
                 var cell = document.createElement("div");
 
                 cell.classList.add("cell");
                 cell.id = "cell-" + id;
-                
+
                 var char = this.getCellValue(id);
                 var cellNumber = Math.floor(char / 4) - 1
-                
+
                 var cellLabel = document.createElement("span");
                 cellLabel.classList.add("cell-label");
                 cellLabel.innerText = ""
@@ -183,10 +267,10 @@ class Puzzle {
         var clickedCell = document.getElementById("cell-" + cellId);
         var color = cellBtn.id.split("-")[0];
         if (color === 'red') {
-            if (!clickedCell.classList.contains("cell-UR")) {
+            if ((!clickedCell.classList.contains("cell-UR")) && !this.#isCreativeMode) {
                 document.getElementById("red-" + cellId).style.backgroundColor = "#00000000";
                 document.getElementById("red-" + cellId).disabled = true;
-                
+
                 this.#mistakeCallback(this.#mistakes);
                 this.#mistakes += 1;
                 return;
@@ -195,13 +279,14 @@ class Puzzle {
             clickedCell.classList.remove("cell-UR");
             clickedCell.removeChild(document.getElementById("red-" + cellId));
             clickedCell.removeChild(document.getElementById("blue-" + cellId));
+            this.#shownCellsIDs.push([cellId, color]);
             this.#completedCells += 1;
         }
         if (color === 'blue') {
-            if (!clickedCell.classList.contains("cell-UB")) {
+            if ((!clickedCell.classList.contains("cell-UB")) && !this.#isCreativeMode) {
                 document.getElementById("blue-" + cellId).style.backgroundColor = "#00000000";
                 document.getElementById("blue-" + cellId).disabled = true;
-                
+
                 this.#mistakeCallback(this.#mistakes);
                 this.#mistakes += 1;
                 return;
@@ -211,6 +296,7 @@ class Puzzle {
             clickedCell.removeChild(document.getElementById("red-" + cellId));
             clickedCell.removeChild(document.getElementById("blue-" + cellId));
             this.#completedCells += 1;
+            this.#shownCellsIDs.push([cellId, color]);
         }
         if (this.#completedCells === this.getSize() ** 2) {
             setTimeout(this.#winCallback, 100);
