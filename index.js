b382f6e61e0f2e598605302afb2cfcb34e9c5dc0
tonyxforce <horich.dominik@gmail.com>
2025-12-03 08:05:32 +0100

    Auto get random level if none are specified

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var url = new URL(document.URL);
+
 var gameString = url.searchParams.get("game");
+
+if (!gameString) {
+    puzzles = levels["6"]; // 6 for 6x6 puzzles
+    gameString = puzzles[Math.floor(Math.random() * puzzles.length)];
+}
 
 var puzzle = new Puzzle(gameString);
 
@@ -9,6 +15,7 @@ puzzle.render("gameHolder");
 puzzle.setWinCallback(() => {
     alert("You win with " + puzzle.getMistakeCount() + " mistakes.");
 });
+
 puzzle.setMistakeCallback(()=>{
     alert("Wrong move!");
 })
