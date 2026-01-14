1efeb1f7a6abd7ad3120c8baa6e65e658d20654f
tonyxforce <horich.dominik@gmail.com>
2026-01-14 23:20:00 +0100

    Generate new random puzzle when none is given

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,40 @@
 import { Puzzle } from "./puzzle.js";
+import { UrjoGenerator } from "./generator.js";
 
 var url = new URL(document.URL);
 
-var gameString = url.searchParams.get("game");
 
-if(!gameString){
-    gameString = "$0"
+
+function setNewPuzzle() {
+
+    var gameString = url.searchParams.get("game");
+
+    if (!gameString) {
+        const generator = new UrjoGenerator();
+        generator.initBoard(4, 4);
+        const generatedBoard = generator.createPuzzle({
+            numberOfNumbers: 1,
+            contradictionCount: 2,
+            identicalChecks: true
+        });
+        console.log(generatedBoard.toString());
+        console.log(generatedBoard.toUrl());
+        gameString = generatedBoard.toUrl();
+    }
+
+    const puzzle = new Puzzle(gameString);
+
+    puzzle.render("gameHolder");
+
+    puzzle.setWinCallback(() => {
+        alert("You win with " + puzzle.getMistakeCount() + " mistakes.");
+        setNewPuzzle();
+    });
+
+    puzzle.setMistakeCallback(() => {
+        alert("Wrong move!");
+    })
 }
 
-const puzzle = new Puzzle(gameString);
+setNewPuzzle();
 
-//puzzle.shuffle();
-puzzle.render("gameHolder");
-
-puzzle.setWinCallback(() => {
-    alert("You win with " + puzzle.getMistakeCount() + " mistakes.");
-});
-
-puzzle.setMistakeCallback(()=>{
-    alert("Wrong move!");
-})
